5805731c3bdd459a0b49d9517aa1644b0b8cbf1b
Tiendev23 <tienimgvideo@gmail.com>
2024-06-13 11:45:42 +0700

    init all

---
+++ F8_JavaScriptNangCao/JS_2_Hoisting_StrictMode_DataType/js_1_Hoisting/app.js
@@ -0,0 +1,30 @@
+// <<<<< Closure >>>>>
+// 3. Hoisting (Đưa lên trên)
+// > Đưa phần khai báo lên đầu phạm vi
+var age 
+console.log(age);
+age= 16
+console.log(age);
+/////////////////////////////////////////////
+{
+    console.log(fullname);//app.js:10 Uncaught ReferenceError: Cannot access 'fullname' before initialization
+    let fullname ='Nguyen'
+}
+
+
+
+
+
+
+
+
+
+
+// 4. Strict mode (Chế độ nghiêm ngặc)
+// 5. This keyword
+// 6. Bind methods
+// 7. Call methods
+// 8. Apply methods
+// 9. Catching methods, thowing errors (Xử lý lỗi)
+// 10. Promise
+// 11. Async Function, await
